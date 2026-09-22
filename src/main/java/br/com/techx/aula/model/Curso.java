98f2b90ff5d24f8c05d4e03b1d8b509b0f77535c
LucasBorges_ads <lucasborgesk0@gmail.com>
2026-09-22 20:25:18 -0300

    Update Curso.java

--- src/main/java/br/com/techx/aula/model/Curso.java
+++ src/main/java/br/com/techx/aula/model/Curso.java
@@ -1,1 +1,20 @@
+package br.com.techx.aula.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Curso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+    private String turno;
+}
