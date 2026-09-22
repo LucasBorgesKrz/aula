5667cd95d4c32b441962349de5ff1532e8e35efc
LucasBorges_ads <lucasborgesk0@gmail.com>
2026-09-22 20:26:24 -0300

    Update CursoRepository.java

--- src/main/java/br/com/techx/aula/repository/CursoRepository.java
+++ src/main/java/br/com/techx/aula/repository/CursoRepository.java
@@ -1,1 +1,7 @@
+package br.com.techx.aula.repository;
 
+import br.com.techx.aula.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+}
