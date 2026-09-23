4e38be00ddbfcce65a79f019454289c189ba2b40
LucasBorges_ads <lucasborgesk0@gmail.com>
2026-09-23 20:30:47 -0300

    Add findByRa method to AlunoRepository
    Added method to find Aluno by RA.

--- src/main/java/br/com/techx/aula/repository/AlunoRepository.java
+++ src/main/java/br/com/techx/aula/repository/AlunoRepository.java
@@ -1,9 +1,10 @@
 package br.com.techx.aula.repository;
-
+ 
 import br.com.techx.aula.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno,Long> {
-
-
+ 
+import java.util.Optional;
+ 
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+    Optional<Aluno> findByRa(String ra);
 }
