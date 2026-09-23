0fe4794c2805e07f0a261f8a21fa538007eb8e02
LucasBorges_ads <lucasborgesk0@gmail.com>
2026-09-23 20:31:25 -0300

    Refactor AlunoService methods and add findByRa

--- src/main/java/br/com/techx/aula/service/AlunoService.java
+++ src/main/java/br/com/techx/aula/service/AlunoService.java
@@ -1,5 +1,5 @@
 package br.com.techx.aula.service;
-
+ 
 import br.com.techx.aula.dto.AlunoRequest;
 import br.com.techx.aula.dto.AlunoResponse;
 import br.com.techx.aula.model.Aluno;
@@ -7,38 +7,49 @@ import br.com.techx.aula.repository.AlunoRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+ 
 import java.util.List;
-
+ 
 @Service
 public class AlunoService {
-
-
+ 
     @Autowired
     private AlunoRepository alunoRepository;
-
+ 
     public List<Aluno> buscar(){
         return alunoRepository.findAll();
     }
-
+ 
     public void create(AlunoRequest alunoRequest) {
         Aluno entity = new Aluno();
-
-        BeanUtils.copyProperties(alunoRequest,entity);
-
+        BeanUtils.copyProperties(alunoRequest, entity);
         alunoRepository.save(entity);
     }
-
+ 
     public AlunoResponse findBy(Long id){
         Aluno aluno = alunoRepository
                 .findById(id)
-                .orElseThrow(()-> new RuntimeException("ID NOT FOUND"));
+                .orElseThrow(() -> new RuntimeException("ID NOT FOUND"));
         return new AlunoResponse(aluno.getId(), aluno.getNome(), aluno.getIdade(), aluno.getRa());
     }
-
-    public void update(Long id, AlunoRequest alunoRequest){};
-
-    public void deleteById(Long id){}
-
-
+ 
+    public AlunoResponse findByRa(String ra){
+        Aluno aluno = alunoRepository
+                .findByRa(ra)
+                .orElseThrow(() -> new RuntimeException("RA NOT FOUND"));
+        return new AlunoResponse(aluno.getId(), aluno.getNome(), aluno.getIdade(), aluno.getRa());
+    }
+ 
+    public void update(Long id, AlunoRequest alunoRequest){
+        Aluno entity = alunoRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("ID NOT FOUND"));
+ 
+        BeanUtils.copyProperties(alunoRequest, entity);
+        alunoRepository.save(entity);
+    }
+ 
+    public void deleteById(Long id){
+        alunoRepository.deleteById(id);
+    }
 }
