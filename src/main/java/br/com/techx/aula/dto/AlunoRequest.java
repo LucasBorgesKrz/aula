3cdef4f877ef8ff960800e4e94cf6a2a289301ad
LucasBorges_ads <lucasborgesk0@gmail.com>
2026-09-23 20:29:45 -0300

    Change RA type from int to String in AlunoRequest

--- src/main/java/br/com/techx/aula/dto/AlunoRequest.java
+++ src/main/java/br/com/techx/aula/dto/AlunoRequest.java
@@ -1,16 +1,16 @@
 package br.com.techx.aula.dto;
-
+ 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+ 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class AlunoRequest {
     private String nome;
     private int idade;
-    private int ra;
+    private String ra; //
 }
 
 
